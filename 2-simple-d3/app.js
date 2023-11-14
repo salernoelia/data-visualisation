ac59e1ba4e3f9b4e1c386dbfac6343c6735d7041
salernoelia <putopet@gmail.com>
2023-11-14 15:22:47 +0100

    feat: simple-d3 now is comparing current vs potential

--- 2-simple-d3/app.js
+++ 2-simple-d3/app.js
@@ -15,81 +15,94 @@ async function fetchData() {
 fetchData()
   .then(data => {
 
-    //Extracting data from json file
-    const habitatData = jsonData
-    .filter(item => item.Above_ground_potential_storage >= 100)
-      .map(item => ({
-        
-        habitat: item.Habitat_name,
-        storage: Math.round(item.Above_ground_potential_storage / 1e6) // Convert to millions and round
-      }));
+    // Extracting data from the JSON file
+const habitatData = jsonData
+.filter(item => item.Above_ground_potential_storage >= 100)
+.map(item => ({
+  habitat: item.Habitat_name,
+  potentialStorage: Math.round(item.Above_ground_potential_storage / 1e6), // Convert to millions and round
+  currentStorage: Math.round(item.Above_ground_current_storage / 1e6) // Convert to millions and round
+}));
 
-    // Grouping data by habitat and summing the storage values
-    const groupedData = Array.from(d3.group(habitatData, d => d.habitat), ([key, value]) => ({
-      habitat: key,
-      totalStorage: d3.sum(value, d => d.storage)
-    }));
+// Grouping data by habitat and summing the storage values
+const groupedData = Array.from(d3.group(habitatData, d => d.habitat), ([key, value]) => ({
+habitat: key,
+totalPotentialStorage: d3.sum(value, d => d.potentialStorage),
+totalCurrentStorage: d3.sum(value, d => d.currentStorage),
+}));
 
-    // Set up a color scale
-    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+// Set up a color scale
+const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
-    // Set up the SVG dimensions and margins
-    const margin = { top: 20, right: 20, bottom: 30, left: 100 };
-    const width = 1200 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+// Set up the SVG dimensions and margins
+const margin = { top: 20, right: 20, bottom: 30, left: 100 };
+const width = 1200 - margin.left - margin.right;
+const height = 800 - margin.top - margin.bottom;
 
-    // Create the SVG element
-    const svg = d3.select('body').append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+// Create the SVG element
+const svg = d3.select('body').append('svg')
+.attr('width', width + margin.left + margin.right)
+.attr('height', height + margin.top + margin.bottom)
+.append('g')
+.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    // Set up the scales for x and y axes
-    const xScale = d3.scaleBand()
-      .domain(groupedData.map(d => d.habitat))
-      .range([0, width])
-      .padding(0.1);
+  
+    
 
-    const yScale = d3.scaleLinear()
-      .domain([0, d3.max(groupedData, d => d.totalStorage)])
-      .range([height, 0]);
+// Set up the scales for x and y axes
+const xScale = d3.scaleBand()
+.domain(groupedData.map(d => d.habitat))
+.range([0, width])
+.padding(0.1);
 
-    // Create the x-axis
-    svg.append('g')
-      .attr('transform', 'translate(0,' + height + ')')
-      .call(d3.axisBottom(xScale));
+const yScale = d3.scaleLinear()
+.domain([0, d3.max(groupedData, d => Math.max(d.totalPotentialStorage, d.totalCurrentStorage))])
+.range([height, 0]);
 
-    // Create the y-axis
-    svg.append('g')
-      .call(d3.axisLeft(yScale));
+// Create the x-axis
+svg.append('g')
+.attr('transform', 'translate(0,' + height + ')')
+.call(d3.axisBottom(xScale));
 
-    // Create the bars
-    svg.selectAll('.bar')
-      .data(groupedData)
-      .enter().append('rect')
-      .attr('class', 'bar')
-      .attr('x', d => xScale(d.habitat))
-      .attr('y', height)
-      .attr('width', xScale.bandwidth())
-      .attr('height', 0)
-      .attr('fill', d => colorScale(d.habitat)) // Set the color based on the habitat
-      .transition()
-      .duration(1000)
-      .attr('y', d => yScale(d.totalStorage))
-      .attr('height', d => height - yScale(d.totalStorage));
+// Create the y-axis
+svg.append('g')
+.call(d3.axisLeft(yScale));
 
-    // Optional: Add labels to the bars
-    svg.selectAll('.bar-label')
-      .data(groupedData)
-      .enter().append('text')
-      .attr('class', 'bar-label')
-      .attr('x', d => xScale(d.habitat) + xScale.bandwidth() / 2)
-      .attr('y', d => yScale(d.totalStorage) - 5)
-      .attr('text-anchor', 'middle')
-      .attr('font-family', 'Arial') // Set the font family
-      .attr('font-size', '12px') // Set the font size
-      .text(d => d.totalStorage + " Gt");
+// Create the bars
+svg.selectAll('.bar-group')
+.data(groupedData)
+.enter().append('g')
+.attr('class', 'bar-group')
+.attr('transform', d => `translate(${xScale(d.habitat)}, 0)`)
+.selectAll('.bar')
+.data(d => [
+  { type: 'current', value: d.totalCurrentStorage },
+  { type: 'potential', value: d.totalPotentialStorage }
+])
+.enter().append('rect')
+.attr('class', d => `bar ${d.type}`)
+.attr('x', (d, i) => i * (xScale.bandwidth() / 2)) // Adjust x position for side-by-side bars
+.attr('y', height)
+.attr('width', xScale.bandwidth() / 2)
+.attr('height', 0)
+.attr('fill', d => colorScale(d.type))
+.transition()
+.duration(1000)
+.attr('y', d => yScale(d.value))
+.attr('height', d => height - yScale(d.value));
+
+// Optional: Add labels to the bars
+svg.selectAll('.bar-label')
+  .data(groupedData)
+  .enter().append('text')
+  .attr('class', 'bar-label')
+  .attr('x', d => xScale(d.habitat) + xScale.bandwidth() / 2)
+  .attr('y', d => yScale(d.totalPotentialStorage) - 5)
+  .attr('text-anchor', 'middle')
+  .attr('font-family', 'Arial')
+  .attr('font-size', '12px')
+  .text(d => ` ${d.totalCurrentStorage} Gt, ${d.totalPotentialStorage} Gt`);
+
 
   })
   .catch(error => {
